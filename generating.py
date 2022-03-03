353e44d275d05b3ec01785f8a17a3e836a45da2e
inimesh <ijmeshoulam@gmail.com>
2022-03-03 14:03:27 +0000

    setting upper_bound arg for generator function, adding smooth line plot on top of scatter for plotting function

--- generating.py
+++ generating.py
@@ -4,21 +4,15 @@
 import random
 
 
-def generator():
+def generator(upper_bound):
   """
   returns: a 2D array of i lists of random integers between 0 - 99,999, 
   with lists making up set i containing i elements (i is range 9 - 29,999 in steps of 10),
   """
   set = []
 
-  for i in range(9, 30_000, 10):
+  for i in range(9, upper_bound, 10):
     input = random.sample(range(100_000), i)
     set.append(input)
-
-  # for i in range(3, 6):
-  #   input = random.sample(range(100_000), 10**i)
-  #   set.append(input)
-
-  # set.append(random.sample(range(100_000), 100_000))
 
   return set
--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
 
 def plotter(data):
   """
@@ -16,12 +17,19 @@ def plotter(data):
   x_data = np.array(x_data)
   y_data = np.array(y_data)
 
+  X_Y_Spline = make_interp_spline(x_data, y_data)
+
+  X_ = np.linspace(x_data.min(), x_data.max(), 8)
+  Y_ = X_Y_Spline(X_)
+
   # x_data is input size, y_data is time in seconds
-  a, b = np.polyfit(x_data, y_data, 1)
+  # a, b = np.polyfit(x_data, y_data, 1)
 
   # plt.plot(x_data, y_data)
+  # plt.plot(x_dat-a, a*x_data+b, color='purple', linewidth=2)
+
+  plt.plot(X_, Y_)
   plt.scatter(x_data, y_data, s=1)
-  plt.plot(x_data, a*x_data+b, color='purple', linewidth=2)
 
   plt.xlabel('Input size')
   plt.ylabel('Time (seconds)')
