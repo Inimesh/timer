2995c112b310783865e7e8560ae14f9f16052aed
inimesh <ijmeshoulam@gmail.com>
2022-03-03 10:55:38 +0000

    Adding docstring to generating function

--- generating.py
+++ generating.py
@@ -5,6 +5,10 @@ import random
 
 
 def generator():
+  """
+  returns: a 2D array of n lists of random integers between 0 - 99,999, 
+  with each list containing 10**n elements (n is a range 0-5)
+  """
   set = []
 
   for i in range(6):
