34be288bc66682ccecf08a696e8541f7f1a768c4
inimesh <ijmeshoulam@gmail.com>
2022-03-03 10:30:15 +0000

    Plotting.py plots a graph of Time(s) vs Input Size

--- plotting.py
+++ plotting.py
@@ -1,11 +1,27 @@
-from timing import timer
-import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
-def plotter():
+def plotter(data):
   """
   args: 2D array of time vs input_size data
   return: plot a cartesian graph of the data
   """
-  # uses matplotlib to plot a graph of input size vs time
-  pass
+  x_data = []
+  y_data = []
+
+  for datum in data:
+    x_data.append(datum[0])
+    y_data.append(datum[1])
+
+  # x_data is input size, y_data is time in seconds
+  plt.plot(x_data, y_data)
+  plt.xlabel('Input size')
+  plt.ylabel('Time (seconds)')
+  plt.title('Algorithm Runtime vs. Input size')
+
+  plt.show()
+
+  return True
+
+
+plotter([ [5, 2], [6, 4], [7, 5], [8, 5.5] ])
+
