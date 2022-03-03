db23f009020193c886429bc46dd39e9459734d59
inimesh <ijmeshoulam@gmail.com>
2022-03-03 12:03:45 +0000

    Experimenting with using perf_counter to record time vs process_time, also prepending  a set of datasets of increasing size from 1 through 999

--- generating.py
+++ generating.py
@@ -6,12 +6,17 @@ import random
 
 def generator():
   """
-  returns: a 2D array of n lists of random integers between 0 - 99,999, 
-  with each list containing 10**n elements (n is a range 0-5)
+  returns: a 2D array of i+n lists of random integers between 0 - 99,999, 
+  with lists making up set i containing i elements (i is range 0 - 999),
+  and lists making up set n containing 10**n elements (n is a range 0-5)
   """
   set = []
 
-  for i in range(6):
+  for i in range(1, 1000):
+    input = random.sample(range(100_000), i)
+    set.append(input)
+
+  for i in range(3, 6):
     input = random.sample(range(100_000), 10**i)
     set.append(input)
 
--- timing.py
+++ timing.py
@@ -1,4 +1,4 @@
-from time import process_time
+from time import perf_counter_ns, process_time
 
 def timer(callback, input):
   """
@@ -6,8 +6,10 @@ def timer(callback, input):
   returns: time in seconds (float)
   """
 
-  start = process_time()
+  # start = process_time()
+  start = perf_counter_ns()
   callback(input)
-  end = process_time()
+  # end = process_time()
+  end = perf_counter_ns()
 
-  return end - start
+  return (end - start)*(10**9)
