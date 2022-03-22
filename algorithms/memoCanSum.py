aaff15e071f8a9e61200cbbe547ea124792579c7
inimesh <ijmeshoulam@gmail.com>
2022-03-22 12:13:26 +0000

    Adding memoized recursive memoCanSum() function

---
+++ algorithms/memoCanSum.py
@@ -0,0 +1,18 @@
+def memoCanSum(target, arr, memo=None):
+  memo = {} if memo == None else memo
+  if target in memo: return memo[target]
+  if target == 0: return True
+  if target < 0: return False
+  
+  for num in arr:
+    remainder = target - num
+    memo[remainder] = memoCanSum(remainder, arr, memo)
+    if memo[remainder]: return True
+
+  return False
+
+print(memoCanSum(7, [2, 3])) # True
+print(memoCanSum(7, [5, 3, 4, 7])) # True
+print(memoCanSum(7, [2, 4])) # False
+print(memoCanSum(8, [2, 3, 5])) # True
+print(memoCanSum(300, [7, 14])) # False
